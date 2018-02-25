0ebc876a21293235d49e9d4b350171db2677741a
Jesus Bibieca <jesusbibieca52@gmail.com>
2018-02-25 18:05:18 -0400

    add index route

--- final_project.py
+++ final_project.py
@@ -3,6 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+@app.route('/restaurants')
+def show_restaurants():
+    return 'This page will show all my restaurants'
+
+
 if __name__ == '__main__':
     app.secret_key = 'Secret-key'
     app.debug = True
